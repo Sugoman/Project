80f84b448cf8daa9250d4bfbeeb2576856505a42
Sugoman <rechalexandr@gmail.com>
2024-11-28 22:16:15 +0300

    test

--- Main/obj/Debug/net8.0-windows/Main.AssemblyInfo.cs
+++ Main/obj/Debug/net8.0-windows/Main.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23df3dc1e57cf38f3b09d719852c1178efddf472")]
 [assembly: System.Reflection.AssemblyProductAttribute("Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
