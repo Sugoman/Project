9a5681e58bfd86d74641220ba0f8f0a88941ad21
Sugoman <rechalexandr@gmail.com>
2024-12-04 18:29:08 +0300

    PressureGrid is worked

---
+++ Main/Pages/PressurePage/MyData.cs
@@ -0,0 +1,10 @@
+﻿namespace Main.Pages.PressurePage
+{
+    public class MyData
+    {
+        public string Pressure { get; set; }
+        public string HeartRate { get; set; }
+        public string Date { get; set; }
+    }
+
+}
--- Main/Pages/PressurePage/Pressure.xaml.cs
+++ Main/Pages/PressurePage/Pressure.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OfficeOpenXml;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Main.Pages.PressurePage
 {
@@ -23,11 +13,54 @@ namespace Main.Pages.PressurePage
         public Pressure()
         {
             InitializeComponent();
+            LoadExcelToDataGrid();
         }
-    
-    }
-    public class Pressures
-    {
+
+        private void pressureDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void LoadExcelToDataGrid()
+        {
+            string filePath = "pressure_data.xlsx";
+            try
+            {
+                // Проверяем существование файла
+                if (!File.Exists(filePath))
+                    throw new FileNotFoundException("Файл не найден!");
+
+                // Считываем данные из Excel с помощью EPPlus
+                var dataList = new List<MyData>();
+                using (var package = new ExcelPackage(new FileInfo(filePath)))
+                {
+                    var worksheet = package.Workbook.Worksheets[0]; // Первая вкладка
+                    int rowCount = worksheet.Dimension.Rows;
+                    int colCount = worksheet.Dimension.Columns;
+
+                    // Читаем строки, начиная с первой (или пропустите, если у вас есть заголовок)
+                    for (int row = 2; row <= rowCount; row++)
+                    {
+                        var data = new MyData
+                        {
+                            Pressure = worksheet.Cells[row, 1].Text,
+                            HeartRate = worksheet.Cells[row, 2].Text,
+                            Date = worksheet.Cells[row, 3].Text
+                        };
+                        dataList.Add(data);
+                    }
+                }
+
+                // Привязываем данные к DataGrid
+                pressureDataGrid.ItemsSource = dataList;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+
+
+        }
     }
 
 }
